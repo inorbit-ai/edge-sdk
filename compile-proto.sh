4ccadc92bc19110c8f4db2aad2e89e3c78a34e01
Miguel Angel Garcia <miguel.garcia@gmail.com>
2021-08-12 11:57:21 -0300

    empty

--- compile-proto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Compiles the protobuf definition to Javascript
-
-set -e
-docker run --rm -v `pwd`:/repo --user $(id -u):$(id -g) gwihlidal/protoc:1.0 -I/repo --js_out=import_style=commonjs,binary:/repo/src /repo/inorbit.proto
